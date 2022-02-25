13c274fdf1094e928fe79c281acda9f7861a9b4f
ZhenyaG7 <92757161+ZhenyaG7@users.noreply.github.com>
2022-02-25 09:46:46 +0100

    Add files via upload

---
+++ 1.6.js
@@ -0,0 +1,4 @@
+const readlineSync =require ("readline-sync");
+let num1 = readlineSync.question("Write an integer ");
+let num2 = readlineSync.question ("Write another integer ")
+console.log(  num1 / num2);
