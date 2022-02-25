89eec536c53289a4f49857ae96bceb7823528be6
Nathalie Daoût <nat.dao3@gmail.com>
2022-02-25 10:10:03 +0100

    3.

---
+++ 3.1.js
@@ -0,0 +1,15 @@
+let arrNum1 = [1, 2, 3, 4, 5];
+let arrNum2 = [100, 101, 102];
+let sum1 = 0;
+let sum2 = 0;
+
+for(el of arrNum1){
+    sum1 += el;
+}
+console.log(sum1);
+
+for(el of arrNum2){
+    sum2 += el;
+}
+
+console.log(sum2);
