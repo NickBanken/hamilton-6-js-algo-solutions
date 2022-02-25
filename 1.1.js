ddc432f2e7215b4f15974fd89c4d5d0dc2cd8046
Matthew Hons <devwithmatt@gmail.com>
2022-02-25 10:03:52 +0100

    fix 1.1.js

--- 1.1.js
+++ 1.1.js
@@ -2,4 +2,4 @@ const readlineSync = require("readline-sync");
 //define a variable
 let name = new String(readlineSync.question('What is your name ? '));
 //display it 
-console.log(hey + " " + name );
+console.log("Hey " + name );
